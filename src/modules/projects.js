f9152d835509e39540069bfb82328c799fb58a9d
heyitslauu <laurencecadag1@gmail.com>
2023-09-05 23:48:41 +0800

    Projects, Tasks LocalStorage Implement

--- src/modules/projects.js
+++ src/modules/projects.js
@@ -1,110 +1,10 @@
+import { LocalStorage } from "./localStorage";
+const storage = new LocalStorage();
+
 export class Project {
     constructor(name) {
         this.name = name;
-        this.projects = [
-            {
-                id: 1000,
-                name: "JavaScript Calculator Project",
-                tasks: [
-                    {
-                        id: 11,
-                        title: 'Design UI',
-                        details: 'Create a user-friendly interface for the calculator.',
-                        dueDate: '2023-09-10',
-                        priorityLevel: 3,
-                    },
-                    {
-                        id: 21,
-                        title: 'Implement Basic Functions',
-                        details: 'Add functionality for addition, subtraction, multiplication, and division.',
-                        dueDate: '2023-09-15',
-                        priorityLevel: 2,
-                    },
-                    {
-                        id: 31,
-                        title: 'Advanced Features',
-                        details: 'Incorporate scientific functions like square root and trigonometric calculations.',
-                        dueDate: '2023-09-30',
-                        priorityLevel: 1,
-                    },
-                    {
-                        id: 41,
-                        title: 'Testing and Bug Fixing',
-                        details: 'Thoroughly test the calculator and fix any bugs or issues.',
-                        dueDate: '2023-10-05',
-                        priorityLevel: 2,
-                    },
-                ]
-            },
-            {
-                id: 2000,
-                name: "Python Web Scraper Project",
-                tasks: [
-                    {
-                        id: 110,
-                        title: 'Research and Planning',
-                        details: 'Identify target websites and plan the data scraping process.',
-                        dueDate: '2023-09-08',
-                        priorityLevel: 2,
-                    },
-                    {
-                        id: 210,
-                        title: 'Scraping Implementation',
-                        details: 'Write Python code to scrape data from selected websites.',
-                        dueDate: '2023-09-20',
-                        priorityLevel: 3,
-                    },
-                    {
-                        id: 310,
-                        title: 'Data Parsing and Storage',
-                        details: 'Parse the scraped data and store it in a structured format (e.g., CSV or database).',
-                        dueDate: '2023-09-30',
-                        priorityLevel: 2,
-                    },
-                    {
-                        id: 410,
-                        title: 'Automation and Scheduling',
-                        details: 'Implement automation to regularly update the data and schedule scraping tasks.',
-                        dueDate: '2023-10-10',
-                        priorityLevel: 1,
-                    },
-                ]
-            },
-            {
-                id: 3000,
-                name: "React To-Do List App",
-                tasks: [
-                    {
-                        id: 111,
-                        title: 'Component Setup',
-                        details: 'Create React components for the to-do list, input form, and task items.',
-                        dueDate: '2023-09-12',
-                        priorityLevel: 2,
-                    },
-                    {
-                        id: 211,
-                        title: 'State Management',
-                        details: 'Implement state management to add, delete, and update tasks.',
-                        dueDate: '2023-09-18',
-                        priorityLevel: 3,
-                    },
-                    {
-                        id: 311,
-                        title: 'User Authentication',
-                        details: 'Add user authentication for personalized to-do lists.',
-                        dueDate: '2023-09-28',
-                        priorityLevel: 2,
-                    },
-                    {
-                        id: 411,
-                        title: 'Styling and Testing',
-                        details: 'Apply CSS styles and perform testing to ensure functionality and usability.',
-                        dueDate: '2023-10-05',
-                        priorityLevel: 3,
-                    },
-                ]
-            }
-        ];
+        this.projects = storage.getStorageItem('projects') || [];
         
         this.nextId = 1
     }
@@ -117,6 +17,8 @@ export class Project {
         }
 
         this.projects.push(newProject)
+
+        storage.setStorageItem('projects', this.getProjects())
     }
     // Find which specific project to add the task
     taskToProject(id, formObject) {
@@ -125,7 +27,7 @@ export class Project {
         formObject.id = this.nextId++;
 
         this.projects[index].tasks.push(formObject)
-
+        storage.setStorageItem('projects', this.getProjects())
         return this.projects[index];
     }
     // show specific object in the projects array
@@ -151,8 +53,9 @@ export class Project {
         this.projects[projectIndex].tasks[taskIndex].title = formObject.title;
         this.projects[projectIndex].tasks[taskIndex].details = formObject.details;
         this.projects[projectIndex].tasks[taskIndex].dueDate = formObject.dueDate;
-        this.projects[projectIndex].tasks[taskIndex].priorityLevel = formObject.priority;
-        
+        this.projects[projectIndex].tasks[taskIndex].priority = formObject.priority;
+       
+        storage.setStorageItem('projects', this.getProjects())
         return this.projects[projectIndex];
 
     }
@@ -162,6 +65,9 @@ export class Project {
         const taskIndex = this.projects[projectIndex].tasks.findIndex((task) => task.id == taskId)
         
         this.projects[projectIndex].tasks.splice(taskIndex, 1);
+        
+        storage.setStorageItem('projects', this.getProjects())
+        
         return this.projects[projectIndex];
     }
     
--- src/modules/tasks.js
+++ src/modules/tasks.js
@@ -1,127 +1,10 @@
+import { LocalStorage } from "./localStorage";
+const storage = new LocalStorage();
+
+
 export class Tasks {
     constructor() {
-        this.tasks = [
-            {
-                id: 1000,
-                title: 'Read a Book',
-                details: 'Read the latest novel you purchased.',
-                dueDate: '2023-09-10',
-                priorityLevel: 3,
-            },
-            {
-                id: 2000,
-                title: 'Walk the Dog',
-                details: 'Take your dog for a walk in the park for at least 30 minutes.',
-                dueDate: '2023-09-05',
-                priorityLevel: 1,
-            },
-            {
-                id: 3000,
-                title: 'Grocery Shopping',
-                details: 'Make a list and buy groceries for the week.',
-                dueDate: '2023-09-07',
-                priorityLevel: 2,
-            },
-            {
-                id: 4000,
-                title: 'Write a Journal Entry',
-                details: 'Reflect on your day and write a journal entry about your thoughts and experiences.',
-                dueDate: '2023-09-04',
-                priorityLevel: 3,
-            },
-            {
-                id: 5000,
-                title: 'Exercise Session',
-                details: 'Complete a 30-minute workout session to stay healthy and fit.',
-                dueDate: '2023-09-06',
-                priorityLevel: 2,
-            },
-            {
-                id: 6000,
-                title: 'Work on a Coding Project',
-                details: 'Spend 2 hours working on your personal coding project or learning a new programming language.',
-                dueDate: '2023-09-08',
-                priorityLevel: 1,
-            },
-            {
-                id: 7000,
-                title: 'Prepare for a Meeting',
-                details: 'Review meeting agenda and gather necessary documents for tomorrow\'s meeting.',
-                dueDate: '2023-09-05',
-                priorityLevel: 2,
-            },
-            {
-                id: 8000,
-                title: 'Call a Friend',
-                details: 'Catch up with a friend or family member over the phone for at least 20 minutes.',
-                dueDate: '2023-09-10',
-                priorityLevel: 3,
-            },
-            {
-                id: 9000,
-                title: 'Home Renovation',
-                details: 'Plan and start renovations for the living room.',
-                dueDate: '2023-09-15',
-                priorityLevel: 1,
-            },
-            {
-                id: 10000,
-                title: 'Learn a New Language',
-                details: 'Begin learning a new language for personal or professional growth.',
-                dueDate: '2023-09-20',
-                priorityLevel: 2,
-            },
-            {
-                id: 11000,
-                title: 'Write a Blog Post',
-                details: 'Create an informative blog post on a topic of your choice.',
-                dueDate: '2023-09-12',
-                priorityLevel: 3,
-            },
-            {
-                id: 12000,
-                title: 'Volunteer at a Local Shelter',
-                details: 'Spend a few hours volunteering at an animal shelter or food bank.',
-                dueDate: '2023-09-08',
-                priorityLevel: 2,
-            },
-            {
-                id: 13000,
-                title: 'Hiking Adventure',
-                details: 'Plan a hiking trip to explore a new trail and enjoy the outdoors.',
-                dueDate: '2023-09-18',
-                priorityLevel: 1,
-            },
-            {
-                id: 14000,
-                title: 'Financial Review',
-                details: 'Review your monthly budget and financial goals.',
-                dueDate: '2023-09-11',
-                priorityLevel: 3,
-            },
-            {
-                id: 15000,
-                title: 'Cook a New Recipe',
-                details: 'Try cooking a new and challenging recipe for dinner.',
-                dueDate: '2023-09-09',
-                priorityLevel: 2,
-            },
-            {
-                id: 16000,
-                title: 'Photography Session',
-                details: 'Spend an afternoon taking photographs in your favorite location.',
-                dueDate: '2023-09-14',
-                priorityLevel: 1,
-            },
-            {
-                id: 17000,
-                title: 'Plan a Road Trip',
-                details: 'Plan a road trip to visit a nearby city or tourist destination.',
-                dueDate: '2023-09-25',
-                priorityLevel: 2,
-            },
-        ];
-        
+        this.tasks = storage.getStorageItem('tasks') || [];
         this.nextId = 1// Initialize an id counter
     }
 
@@ -134,9 +17,9 @@ export class Tasks {
             dueDate: formObject.dueDate,
             priorityLevel: formObject.priority,
         };
-
-        // console.log(newTask);
         this.tasks.push(newTask);
+
+        storage.setStorageItem('tasks', this.getTasks())
     }
 
     // Returns the whole object based on the index
@@ -146,14 +29,15 @@ export class Tasks {
     
     // Gets the index and removes from the array
     removeTask(arr, id) {
-        //Find index from the collection
         const index = this.tasks.findIndex((task) => task.id == id);
-
-        //Find index from the current array populated
+    
+        // BUG: Return the array based on the active filter
         const currentArrIndex = arr.findIndex((task) => task.id == id);
-
         this.tasks.splice(index, 1)// Remove from tasks array
-        arr.splice(currentArrIndex, 1)// remove from current populated array
+
+        storage.setStorageItem('tasks', this.getTasks())
+        return this.getTasks();
+
     }
 
     filterTask(category) {
@@ -172,9 +56,6 @@ export class Tasks {
             case 'important':
                 return this.tasks.filter(task => task.priorityLevel == 1)
                 break;   
-            case 'all':
-                return this.tasks;
-                break;    
             default:
               return this.getTasks();
           }
@@ -194,7 +75,7 @@ export class Tasks {
         this.tasks[index].details = formObject.details
         this.tasks[index].dueDate = formObject.dueDate
         this.tasks[index].priorityLevel = formObject.priority
-        
+        storage.setStorageItem('tasks', this.getTasks())
         return this.getTasks();
     }
     formatDate(date) {
