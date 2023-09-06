8dbe392428aa67c1b2d62ad880dc0efee784614d
heyitslauu <laurencecadag1@gmail.com>
2023-09-06 16:34:48 +0800

    DOMContentLoaded init values

--- src/app.js
+++ src/app.js
@@ -1,10 +1,5 @@
 import DomModule from "./modules/dom";
-import { Tasks } from "./modules/tasks";
-import { Project } from "./modules/projects";
 
 document.addEventListener('DOMContentLoaded', () => {
-    const taskManager = new Tasks();
-    const projectManager = new Project();
-    DomModule.renderTasks(taskManager.getTasks());
-    DomModule.renderProjects(projectManager.getProjects());
+    DomModule.initLocalStorage();
 })
