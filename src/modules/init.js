cb6193d5457d3cd2bf66324eaf7bee988c050730
heyitslauu <laurencecadag1@gmail.com>
2023-09-06 16:33:25 +0800

    LocalStorage static values add key-value pairs

--- src/modules/init.js
+++ src/modules/init.js
@@ -1,7 +1,7 @@
 export const projectsInit = [
             {
                 id: 1000,
-                name: "JavaScript Calculator Project",
+                name: "JavaScript Calculator",
                 tasks: [
                     {
                         id: 11,
@@ -9,6 +9,7 @@ export const projectsInit = [
                         details: 'Create a user-friendly interface for the calculator.',
                         dueDate: '2023-09-10',
                         priorityLevel: 3,
+                        done: false
                     },
                     {
                         id: 21,
@@ -16,6 +17,7 @@ export const projectsInit = [
                         details: 'Add functionality for addition, subtraction, multiplication, and division.',
                         dueDate: '2023-09-15',
                         priorityLevel: 2,
+                        done: false
                     },
                     {
                         id: 31,
@@ -23,6 +25,7 @@ export const projectsInit = [
                         details: 'Incorporate scientific functions like square root and trigonometric calculations.',
                         dueDate: '2023-09-30',
                         priorityLevel: 1,
+                        done: true
                     },
                     {
                         id: 41,
@@ -30,12 +33,13 @@ export const projectsInit = [
                         details: 'Thoroughly test the calculator and fix any bugs or issues.',
                         dueDate: '2023-10-05',
                         priorityLevel: 2,
+                        done: true
                     },
                 ]
             },
             {
                 id: 2000,
-                name: "Python Web Scraper Project",
+                name: "Web Scraper",
                 tasks: [
                     {
                         id: 110,
@@ -43,6 +47,7 @@ export const projectsInit = [
                         details: 'Identify target websites and plan the data scraping process.',
                         dueDate: '2023-09-08',
                         priorityLevel: 2,
+                        done: false
                     },
                     {
                         id: 210,
@@ -50,6 +55,7 @@ export const projectsInit = [
                         details: 'Write Python code to scrape data from selected websites.',
                         dueDate: '2023-09-20',
                         priorityLevel: 3,
+                        done: false
                     },
                     {
                         id: 310,
@@ -57,6 +63,7 @@ export const projectsInit = [
                         details: 'Parse the scraped data and store it in a structured format (e.g., CSV or database).',
                         dueDate: '2023-09-30',
                         priorityLevel: 2,
+                        done: true
                     },
                     {
                         id: 410,
@@ -64,12 +71,13 @@ export const projectsInit = [
                         details: 'Implement automation to regularly update the data and schedule scraping tasks.',
                         dueDate: '2023-10-10',
                         priorityLevel: 1,
+                        done: true
                     },
                 ]
             },
             {
                 id: 3000,
-                name: "React To-Do List App",
+                name: "React To-Do List",
                 tasks: [
                     {
                         id: 111,
@@ -77,6 +85,7 @@ export const projectsInit = [
                         details: 'Create React components for the to-do list, input form, and task items.',
                         dueDate: '2023-09-12',
                         priorityLevel: 2,
+                        done: false
                     },
                     {
                         id: 211,
@@ -84,6 +93,7 @@ export const projectsInit = [
                         details: 'Implement state management to add, delete, and update tasks.',
                         dueDate: '2023-09-18',
                         priorityLevel: 3,
+                        done: false
                     },
                     {
                         id: 311,
@@ -91,6 +101,7 @@ export const projectsInit = [
                         details: 'Add user authentication for personalized to-do lists.',
                         dueDate: '2023-09-28',
                         priorityLevel: 2,
+                        done: true
                     },
                     {
                         id: 411,
@@ -98,6 +109,7 @@ export const projectsInit = [
                         details: 'Apply CSS styles and perform testing to ensure functionality and usability.',
                         dueDate: '2023-10-05',
                         priorityLevel: 3,
+                        done: true
                     },
                 ]
             }
@@ -109,6 +121,7 @@ export const tasksInit = [
                 details: 'Read the latest novel you purchased.',
                 dueDate: '2023-09-10',
                 priorityLevel: 3,
+                done: true
             },
             {
                 id: 2000,
@@ -116,6 +129,7 @@ export const tasksInit = [
                 details: 'Take your dog for a walk in the park for at least 30 minutes.',
                 dueDate: '2023-09-05',
                 priorityLevel: 1,
+                done: true
             },
             {
                 id: 3000,
@@ -123,6 +137,7 @@ export const tasksInit = [
                 details: 'Make a list and buy groceries for the week.',
                 dueDate: '2023-09-07',
                 priorityLevel: 2,
+                done: true
             },
             {
                 id: 4000,
@@ -130,6 +145,7 @@ export const tasksInit = [
                 details: 'Reflect on your day and write a journal entry about your thoughts and experiences.',
                 dueDate: '2023-09-04',
                 priorityLevel: 3,
+                done: true
             },
             {
                 id: 5000,
@@ -137,6 +153,7 @@ export const tasksInit = [
                 details: 'Complete a 30-minute workout session to stay healthy and fit.',
                 dueDate: '2023-09-06',
                 priorityLevel: 2,
+                done: true
             },
             {
                 id: 6000,
@@ -144,6 +161,7 @@ export const tasksInit = [
                 details: 'Spend 2 hours working on your personal coding project or learning a new programming language.',
                 dueDate: '2023-09-08',
                 priorityLevel: 1,
+                done: false
             },
             {
                 id: 7000,
@@ -151,6 +169,7 @@ export const tasksInit = [
                 details: 'Review meeting agenda and gather necessary documents for tomorrow\'s meeting.',
                 dueDate: '2023-09-05',
                 priorityLevel: 2,
+                done: false
             },
             {
                 id: 8000,
@@ -158,6 +177,7 @@ export const tasksInit = [
                 details: 'Catch up with a friend or family member over the phone for at least 20 minutes.',
                 dueDate: '2023-09-10',
                 priorityLevel: 3,
+                done: false
             },
             {
                 id: 9000,
@@ -165,6 +185,7 @@ export const tasksInit = [
                 details: 'Plan and start renovations for the living room.',
                 dueDate: '2023-09-15',
                 priorityLevel: 1,
+                done: false
             },
             {
                 id: 10000,
@@ -172,6 +193,7 @@ export const tasksInit = [
                 details: 'Begin learning a new language for personal or professional growth.',
                 dueDate: '2023-09-20',
                 priorityLevel: 2,
+                done: false
             },
             {
                 id: 11000,
@@ -179,6 +201,7 @@ export const tasksInit = [
                 details: 'Create an informative blog post on a topic of your choice.',
                 dueDate: '2023-09-12',
                 priorityLevel: 3,
+                done: false
             },
             {
                 id: 12000,
@@ -186,6 +209,7 @@ export const tasksInit = [
                 details: 'Spend a few hours volunteering at an animal shelter or food bank.',
                 dueDate: '2023-09-08',
                 priorityLevel: 2,
+                done: false
             },
             {
                 id: 13000,
@@ -193,6 +217,7 @@ export const tasksInit = [
                 details: 'Plan a hiking trip to explore a new trail and enjoy the outdoors.',
                 dueDate: '2023-09-18',
                 priorityLevel: 1,
+                done: false
             },
             {
                 id: 14000,
@@ -200,6 +225,7 @@ export const tasksInit = [
                 details: 'Review your monthly budget and financial goals.',
                 dueDate: '2023-09-11',
                 priorityLevel: 3,
+                done: false
             },
             {
                 id: 15000,
@@ -207,6 +233,7 @@ export const tasksInit = [
                 details: 'Try cooking a new and challenging recipe for dinner.',
                 dueDate: '2023-09-09',
                 priorityLevel: 2,
+                done: false
             },
             {
                 id: 16000,
@@ -214,6 +241,7 @@ export const tasksInit = [
                 details: 'Spend an afternoon taking photographs in your favorite location.',
                 dueDate: '2023-09-14',
                 priorityLevel: 1,
+                done: false
             },
             {
                 id: 17000,
@@ -221,5 +249,6 @@ export const tasksInit = [
                 details: 'Plan a road trip to visit a nearby city or tourist destination.',
                 dueDate: '2023-09-25',
                 priorityLevel: 2,
+                done: false
             },
         ];
